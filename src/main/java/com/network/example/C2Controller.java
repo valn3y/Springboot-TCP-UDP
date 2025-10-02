5ba65d9da29883e44086ff061c6cf1edfba6d8b0
Valney Marinho do Nascimento Junior <valney.nj@sidi.org.br>
2025-10-01 21:03:14 -0400

    feat(c2): Add new routes for c2 functionality

---
+++ src/main/java/com/network/example/C2Controller.java
@@ -0,0 +1,10 @@
+package com.network.example;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Component
+@RequestMapping("/c2-controller")
+public class C2Controller {
+    
+}
